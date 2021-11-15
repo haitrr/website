1f3f587f6409e755d8493d3a2c5853e65732aebe
Hai Tran <vroyibg@gmail.com>
2021-11-16 01:10:28 +0700

    fix eslint

--- components/introduction.tsx
+++ components/introduction.tsx
@@ -1,5 +1,6 @@
 import styles from "./introduction.module.scss"
 import classNames from "classnames";
+import React from "react";
 function ContactLinks() {
     //TODO: implement this
     return null;
@@ -13,7 +14,7 @@ export const Introduction: React.FC<Props> = ({className}) => {
     const cn = classNames(className, styles.container)
     return <div className={cn}>
         <div className={styles.hello}>Hello,</div>
-        <div className={styles.hai}>I'm Hai</div>
+        <div className={styles.hai}>{"I'm Hai"}</div>
         <div className={styles.job}>A Software Engineer base in Vietnam</div>
         <ContactLinks/>
     </div>;
